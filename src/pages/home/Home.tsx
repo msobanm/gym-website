cee58129a2fb83983dface9d190707f5a11dffb1
Soban Malik <sm280821@gmail.com>
2024-02-03 20:02:59 +0500

    feat: add hero section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react"
-import Navbar from "../components/navbar/Navbar"
-import Footer from "../components/footer/Footer"
+import Navbar from "../../components/navbar/Navbar"
+import Footer from "../../components/footer/Footer"
+import HeroSection from "./components/HeroSection"
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col justify-between min-h-svh">
       <Navbar />
+      <HeroSection />
       <Footer />
     </div>
   )
