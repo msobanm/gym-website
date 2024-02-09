1fa04b7cf3fb663760a521ff2a23373441895bb7
Soban Malik <sm280821@gmail.com>
2024-02-09 23:26:57 +0500

    fix: navbar css

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,11 +5,14 @@ import Icons from "./components/Icons"
 
 const Navbar = () => {
   return (
-    <nav className="bg-white p-2 fixed z-50 w-full">
-      <div className="container mx-auto grid grid-cols-3 items-center content-center">
-        <ul className="flex gap-12">
+    <nav className="bg-white h-[56px] pt-2 fixed z-50 w-full">
+      <div className="px-8 grid grid-cols-3 items-center content-center">
+        <ul className="flex gap-8 h-full items-center">
           {navList.map((item) => (
-            <li className="text-primary cursor-pointer" key={item.title}>
+            <li
+              className="flex items-center px-4 text-primary cursor-pointer border-b-4 border-transparent hover:border-secondary h-full transition duration-100 ease-in-out"
+              key={item.title}
+            >
               <Link
                 to={item.title === "Home" ? `/` : `${item.title.toLowerCase()}`}
               >
@@ -18,7 +21,10 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <Link to={`/`} className="h-12 w-12 justify-self-center cursor-pointer">
+        <Link
+          to={`/`}
+          className="h-12 w-12 justify-self-center cursor-pointer pb-2"
+        >
           <img src="/logo.png" className="h-full w-full object-contain" />
         </Link>
         <Icons />
