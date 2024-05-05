d31fe78efd7de82fd9dabad748b152985507e19d
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:44 +0500

    feat: integrate products api & show clicked product

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -3,32 +3,17 @@ import ProductItem from "./components/ProductItem"
 import Breadcrumbs from "../../components/Breadcrumbs"
 
 const Product = () => {
-  const product = {
-    id: 4,
-    title: "Mens Casual Slim Fit",
-    price: 15.99,
-    description:
-      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-    rating: {
-      rate: 2.1,
-      count: 430,
-    },
-  }
-
   const [quantity, setQuantity] = useState(1)
 
   const handleAddToCart = () => {
     // Implement the logic to add the product to the cart with the selected quantity
-    console.log(`Added ${quantity} ${product.title} to the cart`)
+    console.log(`Added ${quantity} to the cart`)
   }
 
   return (
     <>
       <Breadcrumbs />
       <ProductItem
-        {...product}
         quantity={quantity}
         setQuantity={setQuantity}
         onClick={handleAddToCart}
--- src/pages/products/components/ProductListItem.tsx
+++ src/pages/products/components/ProductListItem.tsx
@@ -2,7 +2,15 @@ import React, { useState } from "react"
 import { ProductListItemProps } from "../../../utils/types"
 import { useNavigate } from "react-router-dom"
 
-const ProductListItem = ({ id, title, price, image }: ProductListItemProps) => {
+const ProductListItem = ({
+  id,
+  title,
+  image,
+  description,
+  price,
+  category,
+  rating,
+}: ProductListItemProps) => {
   const [isHovered, setIsHovered] = useState(false)
   const navigate = useNavigate()
 
@@ -11,12 +19,16 @@ const ProductListItem = ({ id, title, price, image }: ProductListItemProps) => {
       className="mb-2 p-4 min-w-40 min-h-60 flex flex-col gap-2 items-center cursor-pointer "
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      onClick={() => navigate(`/products/${id}`)}
+      onClick={() =>
+        navigate(`/products/${title}`, {
+          state: { id, title, price, image, description, category, rating },
+        })
+      }
     >
       <div className="w-40 h-60 relative">
         <img src={image} className="w-full h-full object-contain " />
         <button
-          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-primary text-white px-4 p-2 w-3/4 h-10 transition-all duration-200 ease-in-out ${
+          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-primary text-white px-4 p-2 w-3/4 h-10 transition-all duration-200 ease-in-out ${
             isHovered ? "opacity-100" : "opacity-0"
           }`}
           onClick={(event) => {
