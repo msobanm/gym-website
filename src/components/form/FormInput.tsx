eae72c21256b93e972dc07cc50d27735f455e449
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:41 +0500

    feat: add Login page

---
+++ src/components/form/FormInput.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { FormInputProps } from "../../utils/types"
+
+const FormInput = ({
+  label,
+  name,
+  type,
+  placeholder,
+  message,
+  register,
+}: FormInputProps) => (
+  <div className="formInput">
+    <label
+      className="block text-gray-700 text-sm font-bold mb-2"
+      htmlFor={name}
+    >
+      {label}
+    </label>
+    <input
+      className="w-full border border-gray-300 p-2 rounded-md"
+      type={type}
+      placeholder={placeholder}
+      {...register}
+    />
+    {message && <p className="text-red-500 text-xs mt-1">{message}</p>}
+  </div>
+)
+
+export default FormInput
