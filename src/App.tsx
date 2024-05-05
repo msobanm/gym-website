8cfbb0689f1878c6523ea338ee0d52f0393afb97
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:48 +0500

    feat: add scroll restoration and upgrade to data router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Routes, Route } from "react-router-dom"
+import {
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/home"
 import Error from "./pages/error"
@@ -11,23 +17,32 @@ import Register from "./pages/register"
 import useAuth from "./utils/hooks/useAuth"
 import Cart from "./pages/cart"
 
+const router = createBrowserRouter([{ path: "*", element: <Root /> }])
+
 function App() {
+  return <RouterProvider router={router} />
+}
+
+function Root() {
   const { isAuth } = useAuth()
   return (
-    <Routes>
-      <Route path="*" element={<Error />} />
-      {!isAuth && <Route path="/login" element={<Login />} />}
-      {!isAuth && <Route path="/register" element={<Register />} />}
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:title" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollRestoration />
+      <Routes>
+        <Route path="*" element={<Error />} />
+        {!isAuth && <Route path="/login" element={<Login />} />}
+        {!isAuth && <Route path="/register" element={<Register />} />}
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:title" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
