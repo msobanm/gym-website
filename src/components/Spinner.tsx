c2ffba47d6a515ec03f300fdd9edf01ad89d6fc3
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:47 +0500

    feat: add loading spinner and product list error and loading

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Spinner = () => {
+  return (
+    <div className="w-[50px] aspect-[1] animate-[spin-1_0.8s_infinite_linear_alternate,spin-2_1.6s_infinite_linear] rounded-[50%] border-8 border-solid border-secondary"></div>
+  )
+}
+
+export default Spinner
--- src/pages/products/components/ProductList.tsx
+++ src/pages/products/components/ProductList.tsx
@@ -1,12 +1,35 @@
 import React from "react"
 import { ProductListProps } from "../../../utils/types"
 import ProductListItem from "./ProductListItem"
+import Spinner from "../../../components/Spinner"
 
-const ProductList = ({ category, products }: ProductListProps) => {
+const ProductList = ({
+  category,
+  products,
+  isPending,
+  isError,
+  error,
+}: ProductListProps) => {
   const filteredProducts =
     category === "All"
       ? products
       : products.filter((p) => p.category === category.toLowerCase())
+
+  if (isPending) {
+    return (
+      <div className="container flex items-center justify-center">
+        <Spinner />
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div className="container flex items-center justify-center">
+        <span>Error: {error?.message}</span>
+      </div>
+    )
+  }
 
   return (
     <ul className="grid grid-cols-4 gap-5">
