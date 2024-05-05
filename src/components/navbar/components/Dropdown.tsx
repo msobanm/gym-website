52937c2aa645102106cf3734717fb14b50be53a6
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:46 +0500

    feat: add logout functionality

--- src/components/navbar/components/Dropdown.tsx
+++ src/components/navbar/components/Dropdown.tsx
@@ -1,17 +1,30 @@
 import React from "react"
+import useSignOut from "react-auth-kit/hooks/useSignOut"
+import { useNavigate } from "react-router-dom"
 
 type Dropdown = {
   isOpen: boolean
 }
 
 const Dropdown = ({ isOpen }: Dropdown) => {
+  const signOut = useSignOut()
+  const navigate = useNavigate()
+
+  function handleSignOut() {
+    signOut()
+    location.reload()
+    navigate("/")
+  }
   return (
     <ul
       className={`bg-white p-[5px] flex shadow-inner absolute top-[2.8em] left-2 rounded ${
         isOpen ? "" : "hidden"
       }`}
     >
-      <li className="m-0 hover:bg-secondary hover:text-white px-5 py-2 rounded cursor-pointer transition duration-100 ease-in-out">
+      <li
+        onClick={handleSignOut}
+        className="m-0 hover:bg-secondary hover:text-white px-5 py-2 rounded cursor-pointer transition duration-100 ease-in-out"
+      >
         Logout
       </li>
     </ul>
