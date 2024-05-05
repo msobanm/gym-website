81495da5f9e177e787e48330555ac174e88d7a17
msobanm <sm280821@gmail.com>
2024-05-05 17:06:26 +0500

    feat: add responsiveness to home page

--- src/pages/home/components/hero/HeroSection.tsx
+++ src/pages/home/components/hero/HeroSection.tsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="bg-cover bg-center h-screen flex items-center text-white bg-[url('/img/background_.jpg')]">
-      <div className="container mx-auto text-left">
-        <h1 className="text-4xl text-white md:text-6xl font-bold mb-4">
+    <div className="bg-cover bg-center h-svh flex xl:items-center text-white bg-[url('/img/background_.jpg')]">
+      <div className="p-5 py-20 text-left">
+        <h1 className="text-4xl text-white drop-shadow-md md:text-6xl font-bold mb-4">
           Your GOTO Store!
         </h1>
-        <p className="text-lg text-white md:text-xl mb-6">
+        <p className="text-lg text-white drop-shadow-md md:text-xl mb-6">
           You can buy your clothes here :)
         </p>
         <Button title="SHOP NOW" onClick={() => navigate("/products")} />
