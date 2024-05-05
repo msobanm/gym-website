4d5d9cc2e5643d527c4e548d8aacbe2dfa1121bf
Soban Malik <sm280821@gmail.com>
2024-05-06 00:16:45 +0500

    fix: add color and hover to breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,6 +1,19 @@
 import React from "react"
 import { Link, useLocation } from "react-router-dom"
 
+type StyledLinkProps = {
+  route: string
+  name: string
+}
+
+function StyledLink({ route, name }: StyledLinkProps) {
+  return (
+    <Link to={route} className="text-secondary hover:text-secondary-2">
+      {name}
+    </Link>
+  )
+}
+
 const Breadcrumbs = () => {
   const location = useLocation()
   const pathnames = location.pathname.split("/").filter((x) => x)
@@ -17,20 +30,25 @@ const Breadcrumbs = () => {
     <>
       <div className="px-2">
         <div className="flex gap-2 border-b-2 px-2">
-          <Link to="/">Home</Link>
-          <div>/</div>
+          <StyledLink route="/" name="Home" />
+          <div>{">"}</div>
           {pathnames.map((name, index) => {
             const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`
             const isLast = index === pathnames.length - 1
             return (
-              <span key={name}>
-                {isLast ? (
-                  <span>{capitalizeAndReplace(name)}</span>
-                ) : (
-                  <Link to={routeTo}>{capitalizeAndReplace(name)}</Link>
-                )}
-                {!isLast && " / "}
-              </span>
+              <>
+                <span key={name} className="text-secondary">
+                  {isLast ? (
+                    <span>{capitalizeAndReplace(name)}</span>
+                  ) : (
+                    <StyledLink
+                      route={routeTo}
+                      name={capitalizeAndReplace(name)}
+                    />
+                  )}
+                </span>
+                <span>{!isLast && " > "}</span>
+              </>
             )
           })}
         </div>
